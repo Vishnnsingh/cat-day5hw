c3e73aac76c740d71819ea1fe17d9a9545a86409
vishnusingh <kumarvishnu65834@gmail.com>
2024-12-24 21:48:21 +0530

    updated

---
+++ React_Day5_HW_Image_App/src/MyComponents/Header/Header.jsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.css'
+
+const Header  = () => {
+    return (
+        <>
+          <div className={styles.header}>
+            <h1>Image Gallery</h1>
+          </div>
+        </>
+    )
+}
+
+export default Header;
